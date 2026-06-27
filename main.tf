52049d8566a43a1cb7f257347d21e81e2cdf3f79
Rajesh Dangi <dangi.rajesh409@gmail.com>
2026-06-27 20:34:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ locals {
     Project     = var.project_name
     Environment = var.environment
     ManagedBy   = "Terraform"
-    Owner       = "UmeshDangi"
+    Owner       = "RajeshDangi"
     Repo        = "cloudpulse-bootstrap"
   }
 }
